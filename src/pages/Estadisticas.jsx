279d156b94e03690e101a074822f606b552c0eb9
AlbaPCode <albapardo.code@gmail.com>
2024-04-25 09:12:15 +0200

    2 CRUD addeded

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -1,10 +1,34 @@
-import styled from "styled-components";
-export function Estadisticas() {
-  return (
-    <Container>
-      <h1>Estadistica</h1>
-    </Container>
-  );
+import React, { useState, useEffect } from 'react';
+import Departments from './Departments';
+import Employees from './Employees';
+import { deleteToken, getToken } from './Tools/auth-helper.js';
+import { useNavigate } from "react-router-dom";
+import { prefixURLBackend } from './Tools/URLs.js';
+import './CRUD.css';
+
+export default function CRUD() {
+    const navigate = useNavigate();
+
+    const logoutAndGoMain = () => {
+        deleteToken();
+        navigate("/", { replace: true });
+    };
+
+    return (
+        <div>
+            <ul className='navigationBar'>
+                <li>
+                    <a href="" onClick={() => logoutAndGoMain()}>Logout</a>
+                </li>
+            </ul>
+
+            <div className='crudContentDiv'>
+                <br/><br/>
+                <Departments />
+                <br/><br/><br/>
+                <Employees />
+                <br/><br/>
+            </div>
+        </div>
+    );
 }
-const Container = styled.div`
- height:100vh;`;
